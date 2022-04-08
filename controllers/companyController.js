15760b65e5212eaaad9cebc11750c13bc1a1158a
vivek30 <vs1010582@gmail.com>
2022-04-08 21:20:22 +0530

    sort method

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,23 +1,6 @@
-const sortByEstablishDate = (companyArr, order) => {
-  companyArr.forEach(([key, value]) => {
-    if (order === "increase")
-      value.sort(
-        (a, b) =>
-          new Date(a.companyDetails.establishDate) -
-          new Date(b.companyDetails.establishDate)
-      );
-    else if (order === "decrease")
-      value.sort(
-        (a, b) =>
-          new Date(b.companyDetails.establishDate) -
-          new Date(a.companyDetails.establishDate)
-      );
-  });
-};
-
 exports.companyDetails = (req, res) => {
   const { sort } = req.query;
-  console.log(sort);
+  const sortBy = typeof sort === "object" ? [...sort] : [sort];
 
   const companyData = req.body;
 
@@ -29,11 +12,43 @@ exports.companyDetails = (req, res) => {
     });
   });
 
-  if (sort === "establishDate") sortByEstablishDate(companyArr, "increase");
-  if (sort === "-establishDate") sortByEstablishDate(companyArr, "decrease");
+  const companyDataArr = [];
+
+  for (key in companyData) {
+    companyDataArr.push(companyData[key]);
+  }
+
+  let newData = [...companyDataArr].flat();
+
+  sortBy.forEach((val) => {
+    if (val === "establishDate") {
+      newData = newData.sort(
+        (a, b) =>
+          new Date(a.companyDetails.establishDate) -
+          new Date(b.companyDetails.establishDate)
+      );
+    }
+    if (val === "-establishDate") {
+      newData = newData.sort(
+        (a, b) =>
+          new Date(b.companyDetails.establishDate) -
+          new Date(a.companyDetails.establishDate)
+      );
+    }
+    if (val === "key") {
+      newData = newData.sort((a, b) =>
+        a.companyDetails.key.localeCompare(b.companyDetails.key)
+      );
+    }
+    if (val === "-key") {
+      newData = newData.sort((a, b) =>
+        b.companyDetails.key.localeCompare(a.companyDetails.key)
+      );
+    }
+  });
 
   res.status(200).json({
     status: "success",
-    data: companyData,
+    data: newData,
   });
 };
